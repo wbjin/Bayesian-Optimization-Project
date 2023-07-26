a837d91ca561265ebcedaf365d43a764bca2116a
wbjin <wbjin@umich.edu>
2023-07-27 03:28:51 +0900

    gpytorch implementations

--- botest.py
+++ botest.py
@@ -1,69 +1,59 @@
-import os
 import torch
-import plotly
-import numpy as np
+import matplotlib.pyplot as plt
+import gpytorch
+import math
 import plotly.graph_objects as go
-from botorch import fit_gpytorch_model
-from botorch.models import SingleTaskGP, ModelListGP
-from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
-from botorch.acquisition.monte_carlo import qExpectedImprovement
-from botorch.optim import optimize_acqf
 
-def targetFunction(individuals):
-	result = []
-	for x in individuals:
-		result.append(np.exp(-(x-2)**2) + np.exp(-(x - 6)**2/10)+1/(x**2+1))
+
+class ExactGPModel(gpytorch.models.ExactGP):
+	def __init__(self, train_x, train_y, likelihood):
+		super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+		self.mean_module = gpytorch.means.ConstantMean()
+		self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+	def forward(self, x):
+		mean_x = self.mean_module(x)
+		covar_x = self.covar_module(x)
+		return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 	
-	return torch.tensor(result)
-
 def plot(x, y, title):
-	data = go.Scatter(x = x, y = y)
-	fig = go.Figure(data = data)
-	fig.update_layout(title = title)
-	fig.show()
+	plt.plot(x, y)
+	plt.show()
 
-def generateInitialData(n = 10):
-	trainX = torch.rand(10, 1)
-	exactObj = targetFunction(trainX).unsqueeze(-1)
-	bestObservedValue = exactObj.max().item()
-	return trainX, exactObj, bestObservedValue
-
-def getNextPoint(initX, initY, bestObservedValue, bounds, numPoints = 1):
-	singleModel = SingleTaskGP(initX, initY)
-	mll = ExactMarginalLogLikelihood(singleModel.likelihood, singleModel)
-	
-	fit_gpytorch_model(mll)
-	EI = qExpectedImprovement(model = singleModel, best_f = bestObservedValue)
-	candidates, _ = optimize_acqf(acq_function=EI, bounds = bounds, q = numPoints, num_restarts = 200,
-			       					raw_samples = 512) 
-	return candidates	
-
-def optimize(initX, initY, bestInitY, bounds, numRuns = 10):
-	for i in range(numRuns):
-		print("Iteration ", i)
-		newCandidate = getNextPoint(initX, initY, bestInitY, bounds)
-		newResults = targetFunction(newCandidate).unsqueeze(-1)
-
-		print("New point: ", newCandidate)
-		initX = torch.cat([initX, newCandidate])
-		initY = torch.cat([initY, newResults])
-
-		title  = "Iteration " + str(i)
-		bestInitY = initY.max().item()	
-		plot(initX, initY, title)
-		print("Best point: ", bestInitY)
 	
 def main():
-	x = np.linspace(-2.0, 10.0, 100)
-	y = targetFunction(x)
-	plot(x, y, "Target Function")
+	xAxis = torch.linspace(0, 1, 100)
+	yPoints = torch.sin(xAxis * (2 * math.pi)) + torch.randn(xAxis.size()) * math.sqrt(0.04)
 
-	#Random initial data set
-	initX, initY, bestInitY = generateInitialData(20)
-	bounds = torch.tensor([[0.], [10.]])
+	likelihood = gpytorch.likelihoods.GaussianLikelihood()
+	model = ExactGPModel(xAxis, yPoints, likelihood)
+	model.train()
+	likelihood.train()
 
-	optimize(initX, initY, bestInitY, bounds, 10)
+	# Use the adam optimizer
+	optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
 
+	# "Loss" for GPs - the marginal log likelihood
+	mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
 
+	for i in range(10):
+		# Zero gradients from previous iteration
+		optimizer.zero_grad()
+		# Output from model
+		output = model(xAxis)
+		# Calc loss and backprop gradients
+		loss = -mll(output, yPoints)
+		loss.backward()
+		optimizer.step()	
+
+	model.eval()
+	mean = likelihood(model(xAxis)).mean.detach().numpy()
+
+	data = []
+	data.append(go.Scatter(x= xAxis, y= mean))
+	figOpt = go.Figure(data=data)
+	figOpt.add_trace(go.Scatter(x = xAxis, y = yPoints, mode='markers'))
+	figOpt.show()
+			
 if __name__ == "__main__":
 	main()
