0299435d41dcff923541139e0a522b4e6736b018
wbjin <wbjin@umich.edu>
2023-08-21 04:17:25 +0900

    4 task opt

--- multiobjectiveopt.py
+++ multiobjectiveopt.py
@@ -9,24 +9,32 @@ from scipy.stats import norm
 
 import plotly.graph_objects as go
 
-class ExactGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood, kernel):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(kernel)
-    
+import math
+class MultitaskGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, params, numTasks):
+        super(MultitaskGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.MultitaskMean(
+            gpytorch.means.ConstantMean(), num_tasks=numTasks
+        )
+        self.covar_module = gpytorch.kernels.MultitaskKernel(
+            gpytorch.kernels.RBFKernel(), num_tasks=numTasks, rank=1
+        )
+
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
-
+        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
+    
 class Optimizer:
     def __init__(self, X, y):
         self.X = X
         self.y = y
         self.numInput = self.X.size(0)
-        self.params = len(self.X[0])
-        print(self.params)
+        self.numTasks = len(self.y[0])
+        if self.X.dim() == 1:
+            self.params = 1
+        else:
+            self.params = len(self.X[0])
         if len(self.X != 0):
             if self.params == 1:
                 self.domain = [self.X[0].item(), self.X[-1].item()]
@@ -35,6 +43,7 @@ class Optimizer:
                 self.domain = [[self.X[0][0].item(), self.X[-1][0].item()], [self.X[0][1].item(), self.X[-1][1].item()]]
                 self.range1 = self.domain[0][1]-self.domain[0][0]
                 self.range2 = self.domain[1][1]-self.domain[1][0]
+    
     def run(self):
         self.trainGP()
         for i in range(5):
@@ -55,7 +64,7 @@ class Optimizer:
         if kernelInput == "matern":
             self.kernel = gpytorch.kernels.MaternKernel(nu=0.5, ard_nums = self.params) 
         if kernelInput == "sek":
-            self.kernel = gpytorch.kernels.RBFKernel()  
+            self.kernel = gpytorch.kernels.RBFKernel(ard_nums = self.params)  
 
     def setAcquisition(self, acquisitionInput):
         self.acquisition = self.probabilityOfImprovement 
@@ -63,15 +72,16 @@ class Optimizer:
             self.acquisition = self.probabilityOfImprovement 
         
     def trainGP(self, trainIter = 10):
-        self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.model = ExactGPModel(self.X, self.y, self.likelihood, self.kernel)
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
+        self.likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=self.numTasks)
+        self.model = MultitaskGPModel(self.X, self.y, self.likelihood, self.params, self.numTasks)
 
         self.model.train()
         self.likelihood.train()
 
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
-        for i in range(trainIter):
+
+        for i in range(20):
             optimizer.zero_grad()
             output = self.model(self.X)
             loss = -mll(output, self.y)
@@ -132,22 +142,16 @@ class Optimizer:
             self.X = torch.cat((self.X, newX.unsqueeze(0)), 0)
             print(self.X)
     def result(self):
-        if self.target == max:
-            index = self.prediction.argmax()
-        else:
-            index = self.prediction.argmin()
-        results = self.X[index]
-        print(results)
-        return results
+        print(self.prediction)
         
 def main():
     filename = 'testdata/multiparam.xlsx'
     df = pd.read_excel(filename)
     df = df.drop(index = 0)
-    A = torch.tensor(df["A"].to_numpy()[:-4])
-    B = torch.tensor(df["B"].to_numpy()[:-4])
-    C = torch.tensor(df["C"].to_numpy()[:-4])
-    D = torch.tensor(df["d"].to_numpy()[:-4])
+    A = torch.tensor(df["A"].to_numpy()[:23])
+    B = torch.tensor(df["B"].to_numpy()[:23])
+    C = torch.tensor(df["C"].to_numpy()[:23])
+    D = torch.tensor(df["d"].to_numpy()[:23])
     E = torch.tensor(df["E"].to_numpy()[:-4])
     F = torch.tensor(df["F"].to_numpy()[:-4])
     G = torch.tensor(df["G"].to_numpy()[:-4])
@@ -157,7 +161,7 @@ def main():
     K = torch.tensor(df["K"].to_numpy()[:-4])
     L = torch.tensor(df["L"].to_numpy()[:-4])
     M = torch.tensor(df["M"].to_numpy()[:-4])
-    N = torch.tensor(df["N"].to_numpy()[:-4])
+    N = torch.tensor(df["N"].to_numpy()[:-4].astype('float64'))
     O = torch.tensor(df["O"].to_numpy()[:-4])
     P = torch.tensor(df["P"].to_numpy()[:-4])
     Q = torch.tensor(df["Q"].to_numpy()[:-4])
@@ -168,18 +172,16 @@ def main():
     V = torch.tensor(df["V"].to_numpy()[:-4])
     W = torch.tensor(df["W"].to_numpy()[:-4])
 
-    X = torch.cat((
-                A.contiguous().view(A.numel(), 1),
-                B.contiguous().view(B.numel(), 1),
-                C.contiguous().view(C.numel(), 1)),
-                dim=1)
-    y = df["TL"].to_numpy()[:-4]
-    y = y.astype('float64')
-    y = torch.from_numpy(y)
+    X = torch.stack([A, B, C], -1).float()
+    y1 = torch.from_numpy(df["DT"].to_numpy()[:23].astype('float64'))
+    y2 = torch.from_numpy(df["r"].to_numpy()[:23].astype('float64'))
+    y3 = torch.from_numpy(df["R0"].to_numpy()[:23].astype('float64'))
+    y4 = torch.from_numpy(df["TL"].to_numpy()[:23].astype('float64'))
+    y = torch.stack([y1, y2, y3, y4], -1).float()
 
     optimizer = Optimizer(X, y)
     optimizer.setOptimizationTarget("Maximize")
-    optimizer.setKernel("matern")
+    optimizer.setKernel("sek")
     optimizer.setAcquisition("pi")
     optimizer.trainGP()
     optimizer.modelSurrogate()
